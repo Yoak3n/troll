c4a33ba66b9124bf4e042f8cdd3601449860b53e
Yoak3n <3354590676@qq.com>
2025-11-25 00:31:59 +0800

    fix: 修正API常量名称，确保搜索评论功能正常工作

--- viewer/viewer-frontend/src/api/modules/comment.ts
+++ viewer/viewer-frontend/src/api/modules/comment.ts
@@ -2,11 +2,11 @@ import type { CommentView } from "../../types";
 import request from "../../utils/request";
 
 const API = {
-    SEARCH_RANGE: "/comments/search" // ? keyword
+    SEARCH_KEYWORD: "/comments/search" // ? keyword
 } as const;
 
 export const fetchCommentsByKeyword = (keyword: string) => {
-    const uri = API.SEARCH_RANGE + '?keyword=' + encodeURIComponent(keyword);
+    const uri = API.SEARCH_KEYWORD + '?keyword=' + encodeURIComponent(keyword);
     console.log(uri);
     return request.get<any, CommentView>(uri);
 }
